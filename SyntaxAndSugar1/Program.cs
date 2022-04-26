576fdeb127d48427e7b680a087c101ce63773fc9
Chance Gardner <Chantz465@gmail.com>
2022-04-26 10:49:28 -0500

    (first add)

---
+++ SyntaxAndSugar1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SyntaxAndSugar1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var  answer = 4;
+            //string response;
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
+
+
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+        }
+    }
+}
